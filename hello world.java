eff40326d78a2684343ae5ec4b2448e065c69e8b
chanchow9 <50043771+chanchow9@users.noreply.github.com>
2019-09-11 11:31:12 -0700

    Update hello world.java

--- hello world.java
+++ hello world.java
@@ -1,6 +1,7 @@
 *thius is a simple hello world! programme
 public class HelloWorld {
 public static void main(String[] args){
-sysyem.out.println("hello world")
+sysyem.out.println("hello world");
+
    }
  }  
